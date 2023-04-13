8ba5fcbb88be3180ad86bec59326507b78afece2
Weilin471 <wtang1@mail.csuchico.edu>
2023-04-13 13:25:31 -0700

    level1

---
+++ Gauntlet/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 moveInput;
+    public float speed;
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+    private void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.velocity = moveInput * speed;
+    }
+}
